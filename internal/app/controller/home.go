aa1b828ba8e948ffe221d926e3f95ddcd5d2a70a
Clivern <hello@clivern.com>
2018-10-10 19:57:10 +0200

    Add PR labels methods

--- internal/app/controller/home.go
+++ internal/app/controller/home.go
@@ -2,9 +2,26 @@ package controller
 
 import (
     "github.com/gin-gonic/gin"
+    "github.com/clivern/hamster/pkg"
+    "os"
+    "fmt"
 )
 
 func Index(c *gin.Context) {
+    github_api := &pkg.GithubAPI{
+        Token: os.Getenv("GithubToken"),
+        Author: os.Getenv("RepositoryAuthor"),
+        Repository: os.Getenv("RepositoryName"),
+    }
+
+    labels, err := github_api.GetRepositoryIssueLabels(11)
+
+    if err == nil {
+        fmt.Println(labels)
+    }else{
+        fmt.Println(err.Error())
+    }
+
     c.JSON(200, gin.H{
         "status": "ok",
     })
